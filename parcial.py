6792830c55b45248bd1e675e20d02ad5b3925f82
lauracerra <51128055+lauracerra@users.noreply.github.com>
2023-08-31 07:29:57 -0500

    Update parcial.py

--- parcial.py
+++ parcial.py
@@ -1,8 +1,8 @@
 import math
-
+#
 def distancia(p1, p2):
     return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
-
+#
 def pares_cercanos_divide_y_venceras(coordenadas):
     if len(coordenadas) <= 3:
         min_dist = float('inf')
@@ -41,7 +41,7 @@ def pares_cercanos_divide_y_venceras(coordenadas):
             j += 1
 
     return puntos_mas_cercanos, min_dist
-
+#
 def pares_cercanos(*args, **kwargs):
     coordenadas = args[0]
     coordenadas.sort(key=lambda x: x[0])
